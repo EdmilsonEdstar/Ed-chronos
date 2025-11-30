7f52941b857366eaf1696c5064ed10cecf1c4e94
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2025-11-30 15:10:50 +0000

    Variáveis e Reset de CSS do Chronos (nosso app)

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -3,5 +3,5 @@ import style from "./Heading.module.css"
 export function Heading(props){ // props é para adicionar uma propriedade
     return <h1 className={style.heading}>{props.children}</h1> 
     // {props.children} serve para atribuir uma valor dentro da tag, ou seja adicionar um children(filho)
-    // Também permite adicionar atributos na tag renderizada.
+    // Também permite adicionar com
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
-import "./global.css"
-import "./theme.css"
+import "./styles/global.css"
+import "./styles/theme.css"
 import { Heading } from './Components/Heading.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Heading>Edmilson</Heading>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi commodi cupiditate porro? Vitae ipsam voluptatibus voluptates quaerat veritatis eum officiis soluta sit. Necessitatibus qui at vel aut consectetur tempore aspernatur?
+    </p>
   </StrictMode>,
 )
