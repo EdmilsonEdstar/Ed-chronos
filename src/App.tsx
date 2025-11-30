516d730b0b04227b78be9de9545795979ace344f
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2025-11-30 16:27:28 +0000

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { TimerIcon } from 'lucide-react'
+import {Heading} from './Components/Heading'
+import "./styles/global.css"
+import "./styles/theme.css"
+
 export function App(){
 
   return(
   <>
     <div>
-          <h1>Olá mundo</h1>
+          <Heading>
+              Olá mundo 
+              <button type="button" aria-label="kkk">
+                  <TimerIcon/>
+              </button>
+          </Heading>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ut quae amet. Porro blanditiis culpa debitis totam mollitia illum sapiente dolore velit delectus nostrum quae, fugiat reprehenderit ipsa eum veritatis!
+          </p>
     </div>
   </>
   )
--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -1,7 +1,12 @@
+import type React from "react";
 import style from "./Heading.module.css"
 
-export function Heading(props){ // props é para adicionar uma propriedade
-    return <h1 className={style.heading}>{props.children}</h1> 
+type HeadingProps ={    // Definir que o componente ou a função Heading, 
+    children : React.ReactNode;  // tem um elemento string, neste caso o children
+}
+
+export function Heading(props: HeadingProps){ // props é para adicionar uma propriedade que recebe o tipo do elemento children(é um atributo da propriedade).
+    return <h1 className={style.heading}>{props.children}</h1>
     // {props.children} serve para atribuir uma valor dentro da tag, ou seja adicionar um children(filho)
-    // Também permite adicionar com
+    // Também permite adicionar atributos.
 }
