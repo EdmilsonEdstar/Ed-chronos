c173db97847031be9ddf3e7b48444034d589d7a3
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2025-12-01 11:26:09 +0000

    Criando o formulário da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/theme.css"
 import {Container} from "./Components/Container"
 import {Menu} from "./Components/Menu"
 import {Logo} from "./Components/Logo"
+import { CountDown } from "./Components/CountDown"
 
 export function App(){
 
@@ -15,6 +16,29 @@ export function App(){
 
     <Container>
         <Menu />
+    </Container>
+
+    <Container>
+        <CountDown />
+    </Container>
+
+    <Container>
+        <form action="" className="form">
+            <div className="formrow">
+               <label htmlFor="input">Task</label>
+               <input type="text" id="input" />
+            </div>
+            <div className="formrow">
+               <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="formrow">
+               <p>Clip</p>
+               <p>0 0 0 0 0 </p>
+            </div>
+            <div className="formrow">
+               <button>Enviar</button>
+            </div>
+        </form>
     </Container>
 
 
---
+++ src/Components/CountDown/index.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import style from "./style.module.css"
+
+
+export function CountDown(){ 
+    return( 
+        <div className={style.countdown}>
+            00:00
+        </div>
+
+    )
+}
